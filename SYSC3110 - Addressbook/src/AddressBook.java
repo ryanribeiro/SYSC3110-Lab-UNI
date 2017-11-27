dbadd9170e1de65fc230037f1d5c972c24ba7d3e
ryanribeiro <rmr@ryanribeiro.com>
2017-11-27 10:03:44 -0500

    Started implementing GUI
    - added a frame, a menu bar, two menus, and four menu items

--- SYSC3110 - Addressbook/src/AddressBook.java
+++ SYSC3110 - Addressbook/src/AddressBook.java
@@ -1,9 +1,17 @@
 import java.util.*;
-public class AddressBook {
+import javax.swing.*;
+
+public class AddressBook extends JFrame{
 	private ArrayList<BuddyInfo> buddyInfo;
+	private JFrame frame;
+	private JMenuBar menuBar;
+	private JMenu addressBookMenu, buddyInfoMenu;
+	private JMenuItem create, save, display, add;
+	
 	
 	public AddressBook () {
 		buddyInfo = new ArrayList<BuddyInfo>();
+		initGUI();
 	}
 	
 	public void addBuddy(BuddyInfo newBuddy) {
@@ -21,5 +29,31 @@ public class AddressBook {
 		AddressBook testbook = new AddressBook();		
 		testbook.addBuddy(test);
 		testbook.removeBuddy(0);
+	}
+	
+	private void initGUI() {
+		frame = new JFrame();
+		menuBar = new JMenuBar();
+		addressBookMenu = new JMenu("Address Book");
+		buddyInfoMenu = new JMenu("BuddyInfo");
+		create = new JMenuItem("Create");
+		save = new JMenuItem("Save");
+		display = new JMenuItem("Display");
+		add = new JMenuItem("Add");
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 400);
+		frame.setLocationRelativeTo(null);		
+		
+		frame.setJMenuBar(menuBar);
+		menuBar.add(addressBookMenu);
+		menuBar.add(buddyInfoMenu);
+		addressBookMenu.add(create);
+		addressBookMenu.add(save);
+		addressBookMenu.add(display);
+		buddyInfoMenu.add(add);
+		
+		frame.setVisible(true);
 	}
 }
